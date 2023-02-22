83bcdcf70b620c01268628420273241b02fbf07d
AlexanderFeed <alex1mishchenko@gmail.com>
2023-02-22 11:58:23 +0300

    Update First3.rb

--- First3.rb
+++ First3.rb
@@ -1,3 +1,4 @@
+#находит минимальный элемент массива
 def min_elem(arr)
 	min = arr[0]
 	for i in arr
@@ -9,7 +10,7 @@ def min_elem(arr)
 	end
 	return min
 end
-
+#находит индекс первого положительного элемента массива
 def perv_polog(arr)
 	for i in 0..arr.length
 		if arr[i].to_i>0
@@ -17,8 +18,10 @@ def perv_polog(arr)
 		end
 	end
 end
+#Считываем из файла
 file = File.new(ARGV[1], "r:UTF-8")
 content = file.read.split
+#Принимаем аргументы программы
 if ARGV[0] == '1'
 	puts(min_elem(content))
 elsif ARGV[0] == '2'	
