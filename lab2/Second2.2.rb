5a9aa641c9b84c3997a1a036825ed304179903a0
AlexanderFeed <alex1mishchenko@gmail.com>
2023-03-24 22:52:34 +0300

    вроде 1-7

--- lab2/Second2.2.rb
+++ lab2/Second2.2.rb
@@ -1,5 +1,7 @@
 require_relative 'second'
-options = {id: 1,first_name:"Sasha", last_name: "Kolya", sur_name: "Mishch",phone: "8989",tg: "tg/tg", mail:"@mail", git: "git/git"}
+options = {id: 1,first_name:"Sasha", last_name: "Kolya", sur_name: "Mishch",phone:"89186832818",tg:"tgtg", mail:"@mail", git:"git/git"}
 sasha = Student.new(options)
-p sasha.tg
+p sasha.first_name
+p sasha.last_name
 sasha.tg = "new TG"
+p sasha.tg
--- lab2/second.rb
+++ lab2/second.rb
@@ -1,17 +1,45 @@
 #класс студентов
 class Student
    attr_accessor :id, :first_name, :last_name, :sur_name,:phone,:tg, :mail, :git
-   #attr_reader :name
-   #attr_writer :name
+
    #инициилизирующая функция
-   def initialize(options)
-      self.id = options[:id]
-      self.phone = options[:phone]
-      self.tg = options[:tg]
-      self.mail = options[:mail]
-      self.git = options[:git]   
+   def initialize(hash)
+      if(hash[:first_name] == nil or hash[:last_name] == nil or hash[:sur_name] == nil)
+         raise "ФИО обязательно!"
+      end
+      set_id(hash[:id])
+      set_first_name(hash[:first_name])
+      set_last_name(hash[:last_name])
+      set_sur_name(hash[:patronymic]
+      set_phone(hash[:phone])
+      set_tg(hash[:tg])
+      set_mail(hash[:mail])
+      set_git(hash[:git])
    end
 
+   def set_first_name(new_name) 
+         if new_name.match(/^[a-zA-Z]+$/)
+            @first_name = new_name.capitalize
+         else
+            raise "Pishi NE po Russki"
+         end
+   end
+
+   def set_last_name(new_name) 
+         if new_name.match(/^[a-zA-Z]+$/)
+            @last_name = new_name.capitalize
+         else
+            raise "Pishi NE po Russki"
+         end
+   end
+
+   def set_sur_name(new_name) 
+         if new_name.match(/^[a-zA-Z]+$/)
+            @sur_name = new_name.capitalize
+         else
+            raise "Pishi NE po Russki"
+         end
+   end
    def self.valid_telephone?(number)
       return true unless (number =~ /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/).nil?
       false
