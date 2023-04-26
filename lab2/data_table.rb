862e083d1dd9d47b71df5d42eaac38f168821beb
AlexanderFeed <alex1mishchenko@gmail.com>
2023-04-26 08:07:11 +0300

    Элементы в DataTable
    Ну и end потерял

--- lab2/data_table.rb
+++ lab2/data_table.rb
@@ -2,10 +2,19 @@ class DataTable
 	def initialize(matrix)
 		@matrix = matrix
 	end
+	def get_element(row, column)
+		return matrix[row][column]
+	end
+
+	def columns()
+		return self.matrix[0].length
+	end
+
+	def rows()
+		return self.matrix.length
+	end
+
+	private
+
+	attr_reader :matrix
 end
-
-
-
-private
-
-attr_reader :matrix
--- lab2/main.rb
+++ lab2/main.rb
@@ -1,5 +1,6 @@
 require_relative 'student'
 require_relative 'StudentShort'
+require_relative 'data_table'
 options = {id: '1',first_name:"Sasha", last_name: "Kolya", sur_name: "Mishch",phone:"89186832818",tg:"@tgtg", mail:"kek@mail.ru", git:"https://github.com/AlexanderFeed/"}
 #sasha = Student.new(hash:{id: '1',first_name:"Sasha", last_name: "Kolya", sur_name: "Mishch",phone:"89186832818",tg:"@tgtg", mail:"kek@mail.ru", git:"https://github.com/AlexanderFeed/"})
 #stud2 = Student.new({id: '1',first_name:"Kolya", last_name: "Sasha", sur_name: "Kish", phone:"89991231245", git:"https://github.com/AlexanderFeed/"})
@@ -16,8 +17,12 @@ p object0.tg()
 p object0.phone()
 p object0.get_info()
 
-students = Student.read_from_txt("in_t.txt")
+#students = Student.read_from_txt("in_t.txt")
 
-students.each { |obj| puts obj.get_info }
+#students.each { |obj| puts obj.get_info }
 
-Student.write_to_txt("out_t.txt", students)
+#Student.write_to_txt("out_t.txt", students)
+
+matric = [[9,8,7,6],[5,4,3,2]]
+raz = DataTable.new(matric)
+p raz.get_element(0,2)
