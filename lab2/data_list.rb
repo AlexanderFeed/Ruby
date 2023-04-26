36b04f5cff82cff015d44a38413e96d048346bf7
AlexanderFeed <alex1mishchenko@gmail.com>
2023-04-26 08:21:50 +0300

    data_list методы
    Не понял шо нужно с get_names и get_data

--- lab2/data_list.rb
+++ lab2/data_list.rb
@@ -2,6 +2,22 @@ class DataList
 
 	def initialize(array)
 		@array = array
+		@mark = []
+	end
+
+	def select(number)
+		self.mark.append(self.array[number])
+	end
+
+	def get_selected()
+		return self.mark
+	end
+
+	def get_names()
+
+	end
+
+	def get_data()
 	end
 
 	private
--- lab2/main.rb
+++ lab2/main.rb
@@ -1,6 +1,7 @@
 require_relative 'student'
 require_relative 'StudentShort'
 require_relative 'data_table'
+require_relative 'data_list'
 options = {id: '1',first_name:"Sasha", last_name: "Kolya", sur_name: "Mishch",phone:"89186832818",tg:"@tgtg", mail:"kek@mail.ru", git:"https://github.com/AlexanderFeed/"}
 #sasha = Student.new(hash:{id: '1',first_name:"Sasha", last_name: "Kolya", sur_name: "Mishch",phone:"89186832818",tg:"@tgtg", mail:"kek@mail.ru", git:"https://github.com/AlexanderFeed/"})
 #stud2 = Student.new({id: '1',first_name:"Kolya", last_name: "Sasha", sur_name: "Kish", phone:"89991231245", git:"https://github.com/AlexanderFeed/"})
@@ -23,6 +24,12 @@ p object0.get_info()
 
 #Student.write_to_txt("out_t.txt", students)
 
-matric = [[9,8,7,6],[5,4,3,2]]
-raz = DataTable.new(matric)
-p raz.get_element(0,2)
+#matric = [[9,8,7,6],[5,4,3,2]]
+#raz = DataTable.new(matric)
+#p raz.get_element(0,2)
+
+arr = [1,2,3,4,5]
+dva = DataList.new(arr)
+dva.select(1)
+dva.select(2)
+p dva.get_selected()
